bf557444c9e7a38dcd04c5897087f492776a51b4
Ravindra Reddy <reddyravindra947@gmail.com>
2022-09-12 08:53:21 +0530

    project created, file setup done, navbar created

---
+++ src/components/Contact.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Contact = () => {
+    return(
+        <div 
+            className="ui raised very padded text container segment"
+            style={{ marginTop: '80px' }}>
+
+            <h3 className="ui header">Contact</h3>
+            <p>Learning React is fun</p>
+
+        </div>
+    )
+}
+
+export default Contact;
---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Home = () => {
+    return(
+        <div 
+            className="ui raised very padded text container segment"
+            style={{ marginTop: '80px' }}>
+
+            <h3 className="ui header">Home</h3>
+            <p>Learning React is fun</p>
+
+        </div>
+    )
+}
+
+export default Home;
