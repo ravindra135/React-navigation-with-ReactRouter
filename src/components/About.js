cc4e25a27df5bee5ebb015a4d4784c564daf28a2
Ravindra Reddy <reddyravindra947@gmail.com>
2022-09-12 20:31:06 +0530

    added modal design in about.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,20 @@
 import React from "react";
-
+import './Modal.css'
 const About = () => {
     return(
-        <div 
-            className="ui raised very padded text container segment"
-            style={{ marginTop: '80px' }}>
+        <div>
+            <div className="modal">
+                <div className="modal-content">
+                    <h3>I am a Modal</h3>
+                </div>
+            </div>
+                <div 
+                className="ui raised very padded text container segment"
+                style={{ marginTop: '80px' }}>
 
-            <h3 className="ui header">About</h3>
-            <p>Learning React is fun</p>
-
+                <h3 className="ui header">About</h3>
+                <p>Learning React is fun</p>
+            </div>
         </div>
     )
 }
