0c7b212c883d0d84927d7d41aff9f7860b596c12
Ravindra Reddy <reddyravindra947@gmail.com>
2022-09-12 10:23:44 +0530

    Link and NavLink added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,14 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
     return(
         <nav className="ui raised very padded segment">
             <a className="ui teal inverted segment" href="/">Gloria</a>
             <div className="ui right floated header">
-                <button className="ui button"><a href="/">Home</a></button>
-                <button className="ui button"><a href="/about">About</a></button>
-                <button className="ui button"><a href="/contact">Contact</a></button>
+                <button className="ui button"><Link to="/">Home</Link></button>
+                <button className="ui button"><NavLink to="/about">About</NavLink></button>
+                <button className="ui button"><NavLink to="/contact">Contact</NavLink></button>
             </div>
         </nav>
     )
