0413be4e813a35fc1ccae533125069931439d36d
Ravindra Reddy <reddyravindra947@gmail.com>
2022-09-12 20:16:53 +0530

    react router dom comp

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Home from "./components/Home";
+import Card from "./components/Card";
 
 function App() {
     return (
@@ -11,9 +12,10 @@ function App() {
             <div className="App">
                 <Navbar />
                 <Routes>
-                    <Route path='/' element={<Home/>} exact/>
+                    <Route path='/' element={<Home />} exact/>
                     <Route path='/about' element={<About/>} />
                     <Route path='/contact' element={<Contact/>} />
+                    <Route path='/card/:user' element={<Card />} />
                 </Routes>
             </div>
         </BrowserRouter>
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,9 @@
 import React from "react";
 
 const Contact = () => {
+
+    
+
     return(
         <div 
             className="ui raised very padded text container segment"
